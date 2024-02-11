72f85cc2b5245f737efc226e1ecf4ba104c9461f
pkdhake@gmail.com <pkdhake@gmail.com>
2024-02-11 08:36:24 +0530

    updates extent, new tc add

--- src/test/java/com/inet/base/BaseClass.java
+++ src/test/java/com/inet/base/BaseClass.java
@@ -1,42 +1,97 @@
 package com.inet.base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.utils.FileUtil;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
     public String baseURL = "https://www.nseindia.com/";
-    public  WebDriver driver;
+    public WebDriver driver;
+    public ExtentHtmlReporter htmlReporter;
+    public ExtentReports extentReports;
+    public ExtentTest extentTest;
 
-    @BeforeClass
+    @BeforeSuite
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Drivers//chromedriver.exe");
         driver = new ChromeDriver();
         //Maximize the screen
         driver.manage().window().maximize();
         //Delete all the cookies
         driver.manage().deleteAllCookies();
 
-       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //PageLoad TimeOuts
-       // driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        // driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
         driver.get(baseURL);
     }
 
     @BeforeTest
-    public void launchBrowser()
-    {
-
+    public void launchBrowser() {
+        setExtent();
     }
-   @AfterTest
+
+    @AfterTest
     public void tearDown() {
-      //  driver.quit();
+        //  driver.quit();
+        entReport();
     }
 
+
+    public void setExtent() {
+        //htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/ExtentReports/Report.html");
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/ExtentReports/Report.html");
+
+        htmlReporter.config().setDocumentTitle("Automation Test Report");
+        htmlReporter.config().setReportName("PD_Test_Report Report");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+        extentReports.setSystemInfo("OS", "Windows 11");
+    }
+
+    @AfterMethod
+    public void tearDown1(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            extentTest.log(Status.FAIL, "ITEST CASE FAILED IS " + result.getName());
+            extentTest.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable());
+            String s = getScreenshot(result.getName());
+            extentTest.addScreenCaptureFromPath(s);
+
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            extentTest.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            extentTest.log(Status.PASS, "Test Case PASSED IS " + result.getName());
+        }
+    }
+
+    public void entReport() {
+        extentReports.flush();
+    }
+
+    public String getScreenshot(String screenshotName) throws IOException {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot)driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        // after execution, you could see a folder "FailedTestsScreenshots" under src folder
+        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+        File finalDestination = new File(destination);
+        FileUtils.copyFile(source, finalDestination);
+        return destination;
+    }
 
 }
--- src/test/java/com/inet/testscripts/Tc1.java
+++ src/test/java/com/inet/testscripts/Tc1.java
@@ -4,6 +4,7 @@ import com.inet.base.BaseClass;
 import com.inet.pageobjects.Derivatives;
 import com.inet.pageobjects.NseHomePage;
 import com.inet.utilities.Utils;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tc1 extends BaseClass {
@@ -11,13 +12,17 @@ public class Tc1 extends BaseClass {
     //TC1 Open NSE > Navigate to Equities > Verify Top 10 Gainers/Loser & Most Active Equities
     @Test(priority = 1,enabled = true)
     public void tc_verify_equities() {
+        extentTest = extentReports.createTest("tc_verify_equities");
         System.out.println("======== Started : tc_verify_equities");
 
         //driver.get(baseURL);
         NseHomePage obj_nsehm = new NseHomePage(driver);
 
+        extentTest.createNode("test_Enable_Disable");
         obj_nsehm.test_Enable_Disable();
         obj_nsehm.click_on_Equities();
+
+        extentTest.createNode("verify_Tabs");
         obj_nsehm.verify_Tabs();
         System.out.println("======== Finished : tc_verify_equities");
     }
@@ -25,6 +30,7 @@ public class Tc1 extends BaseClass {
     //TC1 Open NSE > Navigate to Derivatives > Verify 4 tabs should display
     @Test(priority = 2, enabled = true)
     public void tc_verify_derivatives() {
+        extentTest = extentReports.createTest("tc_verify_derivatives");
         System.out.println("======== Started : tc_verify_derivatives");
 
         //driver.get(baseURL);
@@ -34,5 +40,19 @@ public class Tc1 extends BaseClass {
         obj_derivatives.verify_four_tabs();
         System.out.println("======== Finished : tc_verify_derivatives");
 
+    }
+
+
+    @Test(priority = 3, enabled = true)
+    public void tc_verifyTitle() {
+        extentTest = extentReports.createTest("tc_verifyTitle");
+        Assert.assertEquals("gg", driver.getTitle());
+    }
+
+
+    @Test(priority = 4, enabled = true)
+    public void tc_test() {
+        extentTest = extentReports.createTest("tc_test");
+        Assert.assertTrue(true);
     }
 }
